f7c9623dd9ab7f5c9693337253eb3ceb898d08ac
Craig Fox <craig@craigwfox.com>
2024-11-23 19:54:18 -0600

    most stuff

--- src/js/app.js
+++ src/js/app.js
@@ -33,6 +33,66 @@ function getCompProp(compProp) {
 // Set header height
 // ====---------------====
 const headerHeight = document.querySelector("#page-header").offsetHeight
+const footerHeight = document.querySelector("#page-footer").offsetHeight
 if (headerHeight && body) {
 	body.style.setProperty("--header-height", `${headerHeight}px`)
+}
+if (footerHeight && body) {
+	body.style.setProperty("--footer-height", `${footerHeight}px`)
+}
+
+// ====---------------====
+// Trigger new section
+// ====---------------====
+const sectionList = document.querySelectorAll("[data-in-view]")
+let currentSection = null
+
+function clearInView() {
+	sectionList.forEach(el => {
+		el.setAttribute("data-in-view", false)
+	})
+}
+
+const isElementVisible = ele => {
+	const rect = ele.getBoundingClientRect()
+	return (
+		rect.top <= window.innerHeight / 2 &&
+		rect.bottom <= window.innerHeight * 1.5
+	)
+}
+function setActiveSection() {
+	sectionList.forEach(section => {
+		if (section != currentSection && isElementVisible(section)) {
+			currentSection = section
+			clearInView()
+			section.setAttribute("data-in-view", true)
+		}
+	})
+}
+
+setActiveSection()
+document.querySelector("#main-content").addEventListener("scroll", () => {
+	setActiveSection()
+})
+
+// ====---------------====
+// Modal
+// ====---------------====
+const modalNuts = document.querySelectorAll("[data-toggle='modal']")
+if (modalNuts) {
+	modalNuts.forEach(modalNut => {
+		const modalNutModal = document.getElementById(
+			modalNut.getAttribute("aria-controls")
+		)
+		modalNut.addEventListener("click", e => {
+			e.preventDefault()
+			modalNutModal.classList.toggle("js-active")
+		})
+
+		modalNutModal.querySelector(".close").addEventListener("click", e => {
+			console.log("CLICK")
+			e.preventDefault()
+			modalNutModal.classList.remove("js-active")
+		})
+	})
 }
